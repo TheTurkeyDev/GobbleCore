0a14650470bcfa2f58c64f071c123746b7c6a72e
Turkey <turkey2349@gmail.com>
2017-06-17 20:05:18 -0400

    Added BaseItemFood Added more register methods

--- src/main/java/com/theprogrammingturkey/gobblecore/blocks/BlockLoader.java
+++ src/main/java/com/theprogrammingturkey/gobblecore/blocks/BlockLoader.java
@@ -28,6 +28,11 @@ public class BlockLoader
 		this.tab = tab;
 	}
 
+	public void registerBlock(BaseBlock block)
+	{
+		this.registerBlock(block, block.getBlockName());
+	}
+
 	public void registerBlock(Block block, String name)
 	{
 		block.setRegistryName(subMod.getModID(), name);
@@ -36,10 +41,20 @@ public class BlockLoader
 		GameRegistry.register(new BaseItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
+	public void registerBlock(BaseBlock block, Class<? extends TileEntity> tileEntityClass)
+	{
+		this.registerBlock(block, tileEntityClass, block.getBlockName());
+	}
+
 	public void registerBlock(Block block, Class<? extends TileEntity> tileEntityClass, String name)
 	{
 		registerBlock(block, name);
 		GameRegistry.registerTileEntity(tileEntityClass, "tile_" + name);
+	}
+
+	public void registerBlockModel(ItemModelMesher mesher, BaseBlock b, int meta)
+	{
+		this.registerBlockModel(mesher, b, meta, b.getBlockName());
 	}
 
 	public void registerBlockModel(ItemModelMesher mesher, Block b, int meta, String name)
